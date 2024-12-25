594257a48cc971e35b7cc4c083fe0bcae8f221fc
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-25 18:30:37 +0530

    line

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -28,18 +28,22 @@ class HomePage extends StatelessWidget {
                   padding: const EdgeInsets.all(12),
                   child: SvgPicture.asset('assets/icons/Search.svg'),
                 ),
-                suffixIcon: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    const VerticalDivider(
-                      color: Colors.black,
-                      thickness: 0.1,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SvgPicture.asset('assets/icons/Filter.svg'),
-                    ),
-                  ],
+                suffixIcon: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SvgPicture.asset('assets/icons/Filter.svg'),
+                      ),
+                    ],
+                  ),
                 ),
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15),
